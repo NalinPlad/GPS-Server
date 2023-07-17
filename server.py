aa00cd81c1812e21ceee79e6d835607c12a7dc9b
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2023-07-17 13:47:03 -0700

    change ip addr detection

--- server.py
+++ server.py
@@ -25,8 +25,7 @@ async def main():
 
 # Get ip addr
 import socket
-hostname=socket.gethostname()
-IPAddr=socket.gethostbyname(hostname)
+IPAddr = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
 
 # Print QR Code optimistically
 url = f"https://{IPAddr}/www/"
